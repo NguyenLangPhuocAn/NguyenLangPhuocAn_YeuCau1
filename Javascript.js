e12d7f1110f3d2a4e0bc5386a97a0b0f0d1d5c2e
Nguyễn Lăng Phước An <119149804+NguyenLangPhuocAn@users.noreply.github.com>
2024-03-22 15:50:10 +0700

    22/3

---
+++ Javascript.js
@@ -0,0 +1,14 @@
+var slideIndex = 0;
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("Myslide");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none"; 
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1} 
+  x[slideIndex-1].style.display = "block"; 
+  setTimeout(carousel,4000); 
+}
+if(window.innerWidth<450)
+carousel();
