a94828ceea2cb35d8b7a0a2b92da367859f441d7
Nguyễn Lăng Phước An <119149804+NguyenLangPhuocAn@users.noreply.github.com>
2024-03-25 15:40:13 +0700

    mobile

--- Javascript.js
+++ Javascript.js
@@ -1,5 +1,5 @@
 var slideIndex = 0;
-function carousel() {
+function Feature() {
   var i;
   var x = document.getElementsByClassName("Myslide");
   for (i = 0; i < x.length; i++) {
@@ -8,7 +8,36 @@ function carousel() {
   slideIndex++;
   if (slideIndex > x.length) {slideIndex = 1} 
   x[slideIndex-1].style.display = "block"; 
-  setTimeout(carousel,4000); 
+  setTimeout(Feature,4000); 
 }
-if(window.innerWidth<450)
-carousel();
+function Modern_Consulting() {
+  var i;
+  var x = document.getElementsByClassName("Myslide1");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none"; 
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1} 
+  x[slideIndex-1].style.display = "block"; 
+  setTimeout(Modern_Consulting,4000); 
+}
+slideIndex = 1;
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("Myslide2");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+if(window.innerWidth<450){
+showDivs(slideIndex);
+Feature();
+Modern_Consulting();
+Option_Boder();
+}
